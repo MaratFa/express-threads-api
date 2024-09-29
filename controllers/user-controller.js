341e1c029665729b4f0f225348f5f15c1e1ddbd1
MaratFa <evolution99@rambler.ru>
2024-09-30 01:10:42 +0300

    Edited

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,36 @@
+const { prisma } = require("../prisma/prisma-client");
+const bcrypt = require("bcryptjs");
+const jdenticon = require("jdenticon");
+const path = require("path");
+
 const UserController = {
   register: async (req, res) => {
-    res.send("register");
+    const { email, password, name } = req.body;
+
+    if (!email || !password || !name) {
+      return res.status(400).json({ error: "Все поля обязательны" });
+    }
+
+    try {
+      const existingUser = await prisma.user.findUnique({ where: { email } });
+
+      if (existingUser) {
+        return res.status(400).json({ error: "Пользователь уже существует" });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const png = jdenticon.toPng(name, 200);
+      const avatarName = `${name}_${Date.now()}.png`;
+      const avatarPath = path.join(__dirname, "../uploads", avatarName);
+
+
+
+
+
+
+      
+    } catch (error) {}
   },
   login: async (req, res) => {
     res.send("Login");

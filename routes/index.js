42c148147628d1523fe9707e9e36cffd863828f7
MaratFa <evolution99@rambler.ru>
2024-10-02 00:03:17 +0300

    Edited

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const UserController = require("../controllers/user-controller");
+const { authenticateToken } = require("../middleware/auth");
 
 const uploadDestination = "uploads";
 
-// Показываем, где хранить файлы
+// Показываем, где хранить загруженные файлы
 const storage = multer.diskStorage({
   destination: uploadDestination,
   filename: function (req, file, cb) {
@@ -13,12 +14,12 @@ const storage = multer.diskStorage({
   },
 });
 
-const uploads = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
-router.get("/current", UserController.current);
-router.get("/user/:id", UserController.getUserById);
-router.put("/user/:id", UserController.updateUser);
+router.get("/current", authenticateToken, UserController.current);
+router.get("/user/:id", authenticateToken, UserController.getUserById);
+router.put("/user/:id", authenticateToken, UserController.updateUser);
 
 module.exports = router;

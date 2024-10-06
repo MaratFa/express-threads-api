23a1f06a5a0cb56d2655f7fdfc35f90cffa95542
MaratFa <evolution99@rambler.ru>
2024-10-06 08:39:50 +0300

    edited

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const UserController = require("../controllers/user-controller");
 const { authenticateToken } = require("../middleware/auth");
+const { PostController } = require("../controllers");
 
 const uploadDestination = "uploads";
 
@@ -21,5 +22,11 @@ router.post("/login", UserController.login);
 router.get("/current", authenticateToken, UserController.current);
 router.get("/users/:id", authenticateToken, UserController.getUserById);
 router.put("/users/:id", authenticateToken, UserController.updateUser);
+
+// Роуты постов
+router.post("/posts", authenticateToken, PostController.createPost);
+router.get("/posts", authenticateToken, PostController.getAllPosts);
+router.get("/posts/:id", authenticateToken, PostController.getPostById);
+router.delete("/posts/:id", authenticateToken, PostController.deletePost);
 
 module.exports = router;
